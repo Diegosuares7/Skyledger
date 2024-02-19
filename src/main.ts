96695a81a89f29b5cbbf3a93de5ca44c17c5d8f1
Alejandro Alazraqui <alejandro.alazraqui@dirmod.com>
2024-02-19 22:15:33 +0000

    Merged PR 10844: #50915 : Integro agrupacion de journals a la funcion main
    #50915 : Integro agrupacion de journals a la funcion main
    
    Related work items: #50915

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,18 @@
-import { SkyLedgerReport } from './entities/skyledger-transformed-report/skyledger-report';
+import { GroupedJournals } from './entities/grouper/grouped-journals';
+import { groupJournalsByFileToExport } from './grouper/journal-grouper';
 import { transformXmlToReport } from './transformers/xml.transformer';
 import { readXmlFromAssets } from './xml-parser/xml-reader';
 
 const fileName = `${__dirname}/assets/gl.20240105015614.xml`;
 
-async function executeSkyLedgerIntegration(): Promise<SkyLedgerReport> {
+async function executeSkyLedgerIntegration(): Promise<Map<string, GroupedJournals>> {
   const xmlParsed = await readXmlFromAssets(fileName);
   const skyledgerReport = await transformXmlToReport(
     xmlParsed,
     `${__dirname}/transformers/assets/company-code.config.json`,
   );
-  return skyledgerReport;
+  const groupedJournals = groupJournalsByFileToExport(skyledgerReport);
+  return groupedJournals;
 }
 
 executeSkyLedgerIntegration();
